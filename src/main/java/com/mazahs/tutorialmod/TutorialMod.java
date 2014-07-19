b9ba1d4d6f8799d4f0230a8e956a565f259a0de8
Mazahs <mazman007@gmail.com>
2014-07-19 03:45:52 -0500

    Episodes 6 and 7 changes

--- src/main/java/com/mazahs/tutorialmod/TutorialMod.java
+++ src/main/java/com/mazahs/tutorialmod/TutorialMod.java
@@ -1,14 +1,18 @@
 package com.mazahs.tutorialmod;
 
+import com.mazahs.tutorialmod.handler.ConfigurationHandler;
 import com.mazahs.tutorialmod.proxy.IProxy;
 import com.mazahs.tutorialmod.reference.Reference;
+import com.mazahs.tutorialmod.utility.LogHelper;
+
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 
 public class TutorialMod {
 	
@@ -21,18 +25,21 @@ public class TutorialMod {
 	
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-	
+		
+		ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+		FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+		LogHelper.info("Pre Init Complete!");
 	}
 	    
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
-		    	
+		LogHelper.info("Init Complete!");    	
 	}
 	    
 	    
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent event){
-	   	
+		LogHelper.info("Post Init Complete!");
 	}
 
 }
--- src/main/java/com/mazahs/tutorialmod/reference/Reference.java
+++ src/main/java/com/mazahs/tutorialmod/reference/Reference.java
@@ -7,6 +7,6 @@ public class Reference {
 	public static final String VERSION = "1.7.10-0.0.1";
 	public static final String CLIENT_PROXY_CLASS = "com.mazahs.tutorialmod.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.mazahs.tutorialmod.proxy.ServerProxy";
-	
+	public static final String GUI_FACTORY_CLASS = "com.mazahs.tutorialmod.client.gui.GuiFactory";	
 	
 }
